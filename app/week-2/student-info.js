44954247fcc2ba20bb2dccc5b2812acd51ee6eb1
mitali1206 <mitali.vaid@edu.sait.ca>
2025-09-18 13:41:57 -0600

    Completed JS FOR week2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Mitali Vaid</p>
+      <p>
+        GitHub: <a href="https://github.com/mitali1206/cprg306-assignments">My Repository</a>
+      </p>
+    </div>
+  );
+}
