d3c83c9e64f03d32c3f57eadd4c120dab21bd718
mitali1206 <mitali.vaid@edu.sait.ca>
2025-09-25 10:31:00 -0600

    week3
    assignment

--- app/page.js
+++ app/page.js
@@ -5,6 +5,9 @@ export default function DisplayAssignments() {
     <main>
       <h1>CPRG306- WEB DEV 2 (FALL 2025)</h1>
       <Link href="/week-2">Week 2</Link>
+      <br></br>
+      <Link href="/week-3">Week 3</Link>
+      
     </main>
   )
 };
