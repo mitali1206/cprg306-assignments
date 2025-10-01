5ecda2cb1edf6fee9e0b88aa1e3d281980838933
mitali1206 <mitali.vaid@edu.sait.ca>
2025-10-01 13:20:59 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,8 +6,12 @@ export default function DisplayAssignments() {
       <h1>CPRG306- WEB DEV 2 (FALL 2025)</h1>
       <Link href="/week-2">Week 2</Link>
       <br></br>
+
       <Link href="/week-3">Week 3</Link>
-      
+      <br></br>
+
+      <Link href="/week-4">Week 4</Link>
+
     </main>
   )
 };
